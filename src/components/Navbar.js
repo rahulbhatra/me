431381b51f5bc79ee376f16ecc9c3867c9b06745
Rahul Sharma <rahulsharma@Rahuls-Air.cable.rcn.com>
2021-08-26 17:07:14 -0500

    href changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container">
-                <a className="navbar-brand logo" href="/">Rahul Sharma</a>
+                <a className="navbar-brand logo" href="/me">Rahul Sharma</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <FontAwesomeIcon icon={faBars} style={{color: "#fff"}}/>
                 </button>
@@ -17,19 +17,19 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto justify-content-end container-fluid">
                         <li className="nav-item active">
-                            <a className="nav-link" href="/">SKILLS</a>
+                            <a className="nav-link" href="/me">SKILLS</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">PROJECTS</a>
+                            <a className="nav-link" href="/me">PROJECTS</a>
                         </li>
                         <li className="nav-item active">
-                            <a className="nav-link" href="/">EXPERIENCE </a>
+                            <a className="nav-link" href="/me">EXPERIENCE </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">ABOUT ME</a>
+                            <a className="nav-link" href="/me">ABOUT ME</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">CONTACT</a>
+                            <a className="nav-link" href="/me">CONTACT</a>
                         </li>
                     </ul>
                 </div>
